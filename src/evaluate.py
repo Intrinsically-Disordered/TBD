285d82392af03dd98ca31174cf4005aa78841eb6
Jiawei He <jiaweihe@uw.edu>
2021-12-04 14:31:14 -0800

    Functions to evaluate the model

---
+++ src/evaluate.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+from model import load_data
+
+
+def load_model(infile="fitted_model"):
+    """Load fitted model.
+
+    Args:
+        infile (str, optional): path to saved model. Defaults to "fitted_model".
+
+    Returns:
+        model: fitted model.
+    """
+    model = tf.keras.models.load_model(infile)
+    return model
+
+
+def evaluate_model(model, test_images, test_labels):
+    """Evaluate the model performance
+
+    Args:
+        model ([type]): fitted model
+        test_images (np.ndarray): features for evaluation
+        test_labels (np.ndarray): labels for evaluation
+    """
+    loss, acc, auc = model.evaluate(test_images, test_labels, verbose=2)
+    print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
+    print(model.predict(test_images))
+
+
+def main():
+    model = load_model()
+    print(type(model))
+    data_infile = "../data/protein_processed_data.pkl"
+    X_train, X_test, y_train, y_test = load_data(data_infile)
+    evaluate_model(model, X_test, y_test)
+
+
+if __name__ == "__main__":
+    main()
