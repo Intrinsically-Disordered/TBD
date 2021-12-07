5678391b42afd1f4794216368c89d9ecec02f4dc
Jiawei He <jiaweihe@uw.edu>
2021-12-06 21:16:13 -0800

    Add setup.py and other script

---
+++ setup.py
@@ -0,0 +1,53 @@
+import sys
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    base_dir = Path(__file__).parent
+    src_dir = base_dir/'src'/'tbd'
+
+    sys.path.insert(0, src_dir.as_posix())
+    import __about__ as about
+
+    with (base_dir/'README.rst').open() as f:
+        long_description = f.read()
+
+    install_requirements = [
+        'numpy',
+        'scipy',
+        'pandas',
+        'scikit-learn',
+        'tensorflow',
+    ]
+
+    test_requirements = [
+        'pytest',
+    ]
+
+    doc_requirements = []
+
+    setup(name=about.__title__,
+          version=about.__version__,
+
+          description=about.__summary__,
+          long_description=long_description,
+          license=about.__license__,
+          url=about.__uri__,
+
+          author=about.__author__,
+          author_email=about.__email__,
+
+          package_dir={'': 'src'},
+          package_data={'': ['data/*.csv', 'data/*.pkl', 'fitted_model']},
+          packages=find_packages(),
+          include_package_data=True,
+
+          install_requires=install_requirements,
+          tests_require=test_requirements,
+          extras_require={
+              'docs': doc_requirements,
+              'test': test_requirements,
+              'dev': doc_requirements + test_requirements
+          },
+          zip_safe=False,)
