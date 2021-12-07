7559be80189a49bdc65a6c3ebf10a390c22302f0
Jiawei He <jiaweihe@uw.edu>
2021-12-06 22:45:04 -0800

    Trivial

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,6 +1,4 @@
 """Evaluate the performance of the trained model."""
-# from model import load_data
-# from utils import load_model
 
 
 def evaluate_model(model, test_images, test_labels):
@@ -13,4 +11,3 @@ def evaluate_model(model, test_images, test_labels):
     """
     loss, acc, auc = model.evaluate(test_images, test_labels, verbose=2)
     print('Restored model, accuracy: {:5.2f}%'.format(100 * acc))
-    print(model.predict(test_images))
