6d47a43e2b45ddb757ec5a562110c5d19a00efe6
Jiawei He <jiaweihe@uw.edu>
2021-12-10 22:37:01 -0800

    Import package

--- doc/examples/run_tbd.py
+++ doc/examples/run_tbd.py
@@ -1,10 +1,10 @@
 """Run script to launch the pipeline of 
    data processing, modeling and prediction.
 """
-import evaluate
-import model
-import preprocessing
-import utils
+import tbd.evaluate
+import tbd.model
+import tbd.preprocessing
+import tbd.utils
 
 # Process data
 infile_disordered = "data/disordered_sequences.csv"
@@ -12,19 +12,19 @@ infile_ordered_1 = \
     "data/rcsb_pdb_sequence_9b20c2e6f4e2322c79be67683f6cf968_2501-3856.csv"
 infile_ordered_2 = \
     "data/rcsb_pdb_sequence_9b20c2e6f4e2322c79be67683f6cf968_0001-2500.csv"
-df_disordered = preprocessing.clean_disordered_sequence(infile_disordered)
-df_ordered = preprocessing.clean_ordered_sequence(infile_ordered_1, 
+df_disordered = tbd.preprocessing.clean_disordered_sequence(infile_disordered)
+df_ordered = tbd.preprocessing.clean_ordered_sequence(infile_ordered_1, 
 	infile_ordered_2)
-array_ordered, labels_ordered = utils.encode_data(
+array_ordered, labels_ordered = tbd.utils.encode_data(
     df_ordered, 'ordered'
 )
-array_disordered, labels_disordered = utils.encode_data(
+array_disordered, labels_disordered = tbd.utils.encode_data(
     df_disordered, 'disordered'
 )
 
 # Save processed data
 processed_file = "data/protein_processed_data.pkl"
-preprocessing.save_processed_data(
+tbd.preprocessing.save_processed_data(
     array_ordered, labels_ordered,
     array_disordered, labels_disordered,
     processed_file
@@ -32,10 +32,10 @@ preprocessing.save_processed_data(
 
 # Fit model
 model_name = "fitted_model"
-X_train, X_test, y_train, y_test = model.load_data(processed_file)
-model = model.fit_model(X_train, X_test, y_train, y_test)
+X_train, X_test, y_train, y_test = tbd.model.load_data(processed_file)
+model = tbd.model.fit_model(X_train, X_test, y_train, y_test)
 model.save(model_name)
 
 # Evaluate model
-model = utils.load_model()
-evaluate.evaluate_model(model, X_test, y_test)
+model = tbd.utils.load_model()
+tbd.evaluate.evaluate_model(model, X_test, y_test)
